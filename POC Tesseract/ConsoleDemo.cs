7af3d496726dbc2e8fccf6d609d0121f33813a12
Guillaume Houchou <guillaume.houchou@gmail.com>
2025-04-17 23:00:19 +0200

    Ajoutez des fichiers projet.

---
+++ POC Tesseract/ConsoleDemo.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using Tesseract;
+
+namespace POC_Tesseract
+{
+    internal class ConsoleDemo
+    {
+        public static void Run()
+        {
+            var testImagePath = "C:\\Users\\guill\\Programmation\\dotNET_doc\\POC_Tesseract\\POC Tesseract\\phototest.tif";
+
+            try
+            {
+                using (var engine = new TesseractEngine(@"C:/Users/guill/Programmation/dotNET_doc/POC_Tesseract/POC Tesseract/tessdata/", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(testImagePath))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+                            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+
+                            Console.WriteLine("Text (GetText): \r\n{0}", text);
+                            Console.WriteLine("Text (iterator):");
+                            using (var iter = page.GetIterator())
+                            {
+                                iter.Begin();
+
+                                do
+                                {
+                                    do
+                                    {
+                                        do
+                                        {
+                                            do
+                                            {
+                                                if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+                                                {
+                                                    Console.WriteLine("<BLOCK>");
+                                                }
+
+                                                Console.Write(iter.GetText(PageIteratorLevel.Word));
+                                                Console.Write(" ");
+
+                                                if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word))
+                                                {
+                                                    Console.WriteLine();
+                                                }
+                                            } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+
+                                            if (iter.IsAtFinalOf(PageIteratorLevel.Para, PageIteratorLevel.TextLine))
+                                            {
+                                                Console.WriteLine();
+                                            }
+                                        } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                                    } while (iter.Next(PageIteratorLevel.Block, PageIteratorLevel.Para));
+                                } while (iter.Next(PageIteratorLevel.Block));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+                Console.WriteLine("Unexpected Error: " + e.Message);
+                Console.WriteLine("Details: ");
+                Console.WriteLine(e.ToString());
+            }
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ POC Tesseract/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using Tesseract;
+
+
+var imagePath = "C:\\Users\\guill\\Programmation\\dotNET_doc\\POC_Tesseract\\POC Tesseract\\LoginPage.png";
+using var img = new Bitmap(imagePath);
+
+//using var imgSample = img.Clone(new Rectangle(706, 448, 1210-706, 653-448), img.PixelFormat);
+//
+using var engine = new TesseractEngine("C:\\Users\\guill\\Programmation\\dotNET_doc\\POC_Tesseract\\POC Tesseract\\tessdata\\", "fra", EngineMode.Default);
+//
+//using var container = engine.Process(imgSample);
+//
+//Console.WriteLine("Text inside the part of the image:");
+//Console.WriteLine(container.GetText());
+
+
+using Page page = engine.Process(img);
+var iterator = page.GetIterator();
+iterator.Begin();
+
+do
+{
+    string lineText = iterator.GetText(PageIteratorLevel.TextLine);
+    if (!string.IsNullOrEmpty(lineText) && lineText.Contains("se connecter", StringComparison.OrdinalIgnoreCase))
+    {
+        if (iterator.TryGetBoundingBox(PageIteratorLevel.TextLine, out var rect))
+        {
+            Console.WriteLine($"Found text: {lineText}");
+            Console.WriteLine($"Bounding box: {rect.X1}, {rect.Y1}, {rect.X2}, {rect.Y2}");
+            return;
+        }
+    }
+} while (iterator.Next(PageIteratorLevel.TextLine));
+
