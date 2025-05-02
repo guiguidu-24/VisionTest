3ec1edb4ba25d096bd30f6229ef9b9b7f16db8ad
Guillaume Houchou <guillaume.houchou@gmail.com>
2025-05-03 00:10:24 +0200

    Add view of the capturing tool

---
+++ VSCaptureExtension/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VSCaptureExtension
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "Capture Tool";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("3c791ec2-3093-492f-8a32-e7e2905f0afd")]
+        internal class Pane : ToolkitToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+            }
+        }
+    }
+}
