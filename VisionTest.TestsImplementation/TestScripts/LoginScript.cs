a79dcdbe1ad529efe14ff8662c1cfd6c3f94f96a
Guillaume Houchou <guillaume.houchou@gmail.com>
2025-08-03 14:33:13 +0200

    Clean test implementation

--- VisionTest.TestsImplementation/TestScripts/LoginScript.cs
+++ VisionTest.TestsImplementation/TestScripts/LoginScript.cs
@@ -1,24 +1,27 @@
-﻿using VisionTest.Core.Services;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System;
 using System.Drawing;
+using System.Threading.Tasks;
+using VisionTest.Core.Services;
+using VisionTest.Core.Services.Storage;
 
 namespace VisionTest.TestsImplementation
 {
     internal class LoginScript //TODO Implement a password secured insertion method
     {
-        private TestExecutor appli;
+        private TestExecutor tester;
+        private RepositoryManager repositoryManager = new RepositoryManager(@"C: \Users\guill\Programmation\dotNET_doc\VisionTest\VisionTest.TestsImplementation\");
 
         [SetUp]
         public void Setup()
         {
-            //appli = new TestExecutor(@"C:\Program Files(x86)\Microsoft\Edge\Application\msedge.exe", ["https://ds006g7k:9443/jts/dashboards"]);
-
+            tester = new TestExecutor();
         }
 
         [Test]
-        public void Login()
+        public async Task Login()
         {
-           
+            tester.Click(await repositoryManager.GetByIdAsync(ScreenElements.star) ?? throw new Exception("element does not exist"));
         }
     }
 }
