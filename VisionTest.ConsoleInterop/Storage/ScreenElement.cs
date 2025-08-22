6a7c84f640c97660ccc8a74536a51b7c512decb9
Guillaume Houchou <guillaume.houchou@gmail.com>
2025-08-22 21:41:54 +0200

    Fix console Interop by re adding ScreenElement

---
+++ VisionTest.ConsoleInterop/Storage/ScreenElement.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace VisionTest.ConsoleInterop.Storage;
+
+
+public class ScreenElement
+{
+    public string Id { get; set; } = String.Empty;
+
+    public List<Rectangle> Boxes { get; } = new List<Rectangle>();
+
+    public List<Bitmap> Images { get; } = new List<Bitmap>();
+
+    public List<string> Texts { get; } = new List<string>();
+}
