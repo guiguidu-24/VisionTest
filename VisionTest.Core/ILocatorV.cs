0f0e4ca5683a903f4697b821debae3fa1de6fc3e
Guillaume Houchou <guillaume.houchou@gmail.com>
2026-02-09 21:09:51 +0100

    Add actions to the ILocatorV interface

--- VisionTest.Core/ILocatorV.cs
+++ VisionTest.Core/ILocatorV.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using VisionTest.Core.Utils;
 
 namespace VisionTest.Core;
 
@@ -6,6 +7,15 @@ public interface ILocatorV //TODO : an element can have multiple images of refer
 {
     public Task ClickAsync();
     public Task ClickAsync(TimeSpan timeout);
+
+    public Task RightClickAsync();
+    public Task RightClickAsync(TimeSpan timeout);
+
+    public Task DoubleClickAsync();
+    public Task DoubleClickAsync(TimeSpan timeout);
+
+    public Task HoverAsync();
+    public Task HoverAsync(TimeSpan timeout);
 
     public Task<Rectangle> WaitForAsync();
     public Task<Rectangle> WaitForAsync(TimeSpan timeout);
