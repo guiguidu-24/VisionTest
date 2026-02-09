8823cba9d4d935bc3621b70a9661d693aeb717c7
Guillaume Houchou <guillaume.houchou@gmail.com>
2026-02-09 21:34:54 +0100

    Add actions to rectangle

--- VisionTest.Core/Utils/RectangleExtension.cs
+++ VisionTest.Core/Utils/RectangleExtension.cs
@@ -1,9 +1,36 @@
-﻿namespace VisionTest.Core.Utils;
+﻿using VisionTest.Core.Input;
+
+namespace VisionTest.Core.Utils;
 
 public static class RectangleExtensions
 {
+    private static readonly IMouse _mouse = new Mouse();
+
     public static Point Center(this Rectangle rect)
     {
         return new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+    }
+
+    public static void RightClick(this Rectangle area)
+    {
+        _mouse.MoveTo(area.Center().X, area.Center().Y);
+        _mouse.RightClick();
+    }
+
+    public static void DoubleClick(this Rectangle area)
+    {
+        _mouse.MoveTo(area.Center().X, area.Center().Y);
+        _mouse.DoubleClick();
+    }
+
+    public static void Hover(this Rectangle area)
+    {
+        _mouse.MoveTo(area.Center().X, area.Center().Y);
+    }
+
+    public static void Click(this Rectangle area)
+    {
+        _mouse.MoveTo(area.Center().X, area.Center().Y);
+        _mouse.LeftClick();
     }
 }
