76216dd1f7e7111a964ea599e439d7dba49d04d1
Guillaume Houchou <guillaume.houchou@gmail.com>
2026-02-10 13:22:28 +0100

    Writing an example script and deleting unused test

--- VisionTest.TestsImplementation/TestScripts/Youtube.cs
+++ VisionTest.TestsImplementation/TestScripts/Youtube.cs
@@ -2,6 +2,7 @@
 using VisionTest.Core;
 using VisionTest.Core.Input;
 using VisionTest.Core.Recognition;
+using VisionTest.Core.Utils;
 
 namespace VisionTest.TestsImplementation.TestScripts
 {
@@ -11,22 +12,39 @@ namespace VisionTest.TestsImplementation.TestScripts
         [Test]
         public async Task Run()
         {
-            var firefox = new LocatorV(new Bitmap("C:\\Users\\guill\\Programmation\\dotNET_doc\\VisionTest\\VisionTest.TestsImplementation\\TestScriptData\\Firefox.png"));
+            using var ffx = new Bitmap("C:\\Users\\guill\\Programmation\\dotNET_doc\\VisionTest\\VisionTest.TestsImplementation\\TestScriptData\\Firefox.png");
+            var firefox = new LocatorV(ffx);
+            var lettersOnly = new OcrOptions(whiteListChar: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ", lang: Language.French);
+            var barreDeRecherche = new LocatorV("Rechercher", lettersOnly);
+            var kb = new Keyboard();
+            var title = new LocatorV("Greatest Bluegrass", lettersOnly);
+
+
+
+
             await firefox.ClickAsync();
 
-            var lettersOnly = new OcrOptions(whiteListChar: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ");
-            var barreDeRecherche = new LocatorV("Rechercher", lettersOnly);
-
-            await barreDeRecherche.ClickAsync();
-            var kb = new Keyboard();
+            ScreenElement barreDeRechercheArea = (await barreDeRecherche.WaitForAsync()).Click();
+            barreDeRechercheArea.Click();
+            
 
             kb.TypeText("bluegrass");
+            kb.PressKey(KeyCode.Enter);
 
-            var bluegrass = new LocatorV("bluegrass", lettersOnly);
-            await bluegrass.ClickAsync();
+            await title.HoverAsync();
 
-            var title = new LocatorV("Greatest Bluegrass", lettersOnly);
-            await title.ClickAsync();
+            await Task.Delay(2000);
+
+            barreDeRechercheArea.DoubleClick();
+
+            var bluegrassMusicArea = await (new LocatorV("bluegrass music")).WaitForAsync();
+
+            var interestArea = RectangleFactory.FromPoints(barreDeRechercheArea.Bounds.LowerLeft(), bluegrassMusicArea.Bounds.UpperRight());
+            var bg = new LocatorV("bluegrass", lettersOnly, interestArea);
+
+            var rect = await bg.WaitForAsync();
+
+            rect.Hover();
         }
     }
 }
